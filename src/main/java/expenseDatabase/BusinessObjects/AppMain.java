4ade1859a7b07077b2ed5a83a378748cd1232990
mattjt <mattjpt@gmail.com>
2025-02-16 17:42:47 +0000

    finished expenses
    add, delete, list expenses and total.

--- src/main/java/expenseDatabase/BusinessObjects/AppMain.java
+++ src/main/java/expenseDatabase/BusinessObjects/AppMain.java
@@ -1,13 +1,14 @@
 package expenseDatabase.BusinessObjects;
 
 import expenseDatabase.DAOs.ExpenseDAO;
+import expenseDatabase.DTOs.ExpenseDTO;
 import java.util.Scanner;
 
 public class AppMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         boolean cont = true;
-        while (cont) {
+        while (cont) {//menu
             System.out.println("(1) List all expenses");
             System.out.println("(2) Add new expense");
             System.out.println("(3) Delete expense");
@@ -21,23 +22,34 @@ public class AppMain {
             scan.nextLine();
 
             switch (choice) {
-                case 1:
+                case 1://list expenses and show total
                     ExpenseDAO.listExpenses();
                     break;
-                case 2:
-                    //add new expense
+                case 2://add a new expense
+                    System.out.println("Enter title:");
+                    String title = scan.nextLine();
+                    System.out.println("Enter category:");
+                    String category = scan.nextLine();
+                    System.out.println("Enter amount:");
+                    double amount = scan.nextDouble();
+                    scan.nextLine();
+                    System.out.println("Enter date (YYYY-MM-DD):");//maybe change date format
+                    String date = scan.nextLine();
+                    ExpenseDAO.addExpense(new ExpenseDTO(title, category, amount, date));
                     break;
-                case 3:
-                    //DeleteExpense
+                case 3://Delete Expense by id
+                    System.out.println("Enter expense ID to delete:");
+                    int eId = scan.nextInt();
+                    ExpenseDAO.deleteExpense(eId);
                     break;
                 case 4:
                     //list all income
                     break;
                 case 5:
-                    //addnewincome
+                    //add new income
                     break;
                 case 6:
-                    //deleteincome
+                    //delete income
                     break;
                 case 7:
                     //month summary
--- src/main/java/expenseDatabase/DAOs/ExpenseDAO.java
+++ src/main/java/expenseDatabase/DAOs/ExpenseDAO.java
@@ -1,5 +1,7 @@
 package expenseDatabase.DAOs;
 
+import expenseDatabase.DTOs.ExpenseDTO;
+
 import java.sql.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -28,4 +30,37 @@ public class ExpenseDAO{
             e.printStackTrace();
         }
     }
+
+    public  static void addExpense(ExpenseDTO expense) {
+        String sqlQuery = "INSERT INTO expenses (title, category, amount, dateIncurred) VALUES (?, ?, ?, ?)";
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/financeDB", "root", "");
+             PreparedStatement ps = connection.prepareStatement(sqlQuery)) {
+
+             ps.setString(1,expense.getTitle());
+             ps.setString(2,expense.getCategory());
+             ps.setDouble(3,expense.getAmount());
+             ps.setString(4,expense.getDateIncurred());
+             ps.executeUpdate();
+
+             System.out.println("Expense added.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void deleteExpense(int expenseID) {//id passed in from user input
+        String sqlQuery = "DELETE FROM expenses WHERE expenseID = ?";
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/financeDB", "root", "");
+             PreparedStatement ps = connection.prepareStatement(sqlQuery)) {
+
+             ps.setInt(1, expenseID);
+             ps.executeUpdate();
+
+             System.out.println("Expense deleted.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
